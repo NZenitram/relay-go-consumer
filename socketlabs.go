348773b7d41852cfa666650e663c7e84f9fe2dd6
Nick Martinez <nicholasmartinez@gmail.com>
2024-08-31 16:08:31 -0700

    socketlabs webhook data hitting DB function

--- socketlabs.go
+++ socketlabs.go
@@ -86,89 +86,89 @@ func SendEmailWithSocketLabs(emailMessage EmailMessage) {
 // 	return "Unknown"
 // }
 
-// Base struct for common fields
-type SocketLabsBaseEvent struct {
-	Provider     string `json:"Provider"`
-	Type         string `json:"Type"`
-	DateTime     string `json:"DateTime"`
-	MailingId    string `json:"MailingId"`
-	MessageId    string `json:"MessageId"`
-	Address      string `json:"Address"`
-	ServerId     int    `json:"ServerId"`
-	SubaccountId int    `json:"SubaccountId"`
-	IpPoolId     int    `json:"IpPoolId"`
-	SecretKey    string `json:"SecretKey"`
-}
+// // Base struct for common fields
+// type SocketLabsBaseEvent struct {
+// 	Provider     string `json:"Provider"`
+// 	Type         string `json:"Type"`
+// 	DateTime     string `json:"DateTime"`
+// 	MailingId    string `json:"MailingId"`
+// 	MessageId    string `json:"MessageId"`
+// 	Address      string `json:"Address"`
+// 	ServerId     int    `json:"ServerId"`
+// 	SubaccountId int    `json:"SubaccountId"`
+// 	IpPoolId     int    `json:"IpPoolId"`
+// 	SecretKey    string `json:"SecretKey"`
+// }
 
-// SocketLabsComplaintEvent struct
-type SocketLabsComplaintEvent struct {
-	SocketLabsBaseEvent
-	UserAgent string `json:"UserAgent"`
-	From      string `json:"From"`
-	To        string `json:"To"`
-	Length    int    `json:"Length"`
-}
+// // SocketLabsComplaintEvent struct
+// type SocketLabsComplaintEvent struct {
+// 	SocketLabsBaseEvent
+// 	UserAgent string `json:"UserAgent"`
+// 	From      string `json:"From"`
+// 	To        string `json:"To"`
+// 	Length    int    `json:"Length"`
+// }
 
-// SocketLabsFailedEvent struct
-type SocketLabsFailedEvent struct {
-	SocketLabsBaseEvent
-	BounceStatus   string         `json:"BounceStatus"`
-	DiagnosticCode string         `json:"DiagnosticCode"`
-	FromAddress    string         `json:"FromAddress"`
-	FailureCode    int            `json:"FailureCode"`
-	FailureType    string         `json:"FailureType"`
-	Reason         string         `json:"Reason"`
-	RemoteMta      string         `json:"RemoteMta"`
-	Data           SocketLabsData `json:"Data"`
-}
+// // SocketLabsFailedEvent struct
+// type SocketLabsFailedEvent struct {
+// 	SocketLabsBaseEvent
+// 	BounceStatus   string         `json:"BounceStatus"`
+// 	DiagnosticCode string         `json:"DiagnosticCode"`
+// 	FromAddress    string         `json:"FromAddress"`
+// 	FailureCode    int            `json:"FailureCode"`
+// 	FailureType    string         `json:"FailureType"`
+// 	Reason         string         `json:"Reason"`
+// 	RemoteMta      string         `json:"RemoteMta"`
+// 	Data           SocketLabsData `json:"Data"`
+// }
 
-// SocketLabsTrackingEvent struct
-type SocketLabsTrackingEvent struct {
-	SocketLabsBaseEvent
-	TrackingType int            `json:"TrackingType"`
-	ClientIp     string         `json:"ClientIp"`
-	Url          string         `json:"Url"`
-	UserAgent    string         `json:"UserAgent"`
-	Data         SocketLabsData `json:"Data"`
-}
+// // SocketLabsTrackingEvent struct
+// type SocketLabsTrackingEvent struct {
+// 	SocketLabsBaseEvent
+// 	TrackingType int            `json:"TrackingType"`
+// 	ClientIp     string         `json:"ClientIp"`
+// 	Url          string         `json:"Url"`
+// 	UserAgent    string         `json:"UserAgent"`
+// 	Data         SocketLabsData `json:"Data"`
+// }
 
-// SocketLabsDeliveredEvent struct
-type SocketLabsDeliveredEvent struct {
-	SocketLabsBaseEvent
-	Response  string         `json:"Response"`
-	LocalIp   string         `json:"LocalIp"`
-	RemoteMta string         `json:"RemoteMta"`
-	Data      SocketLabsData `json:"Data"`
-}
+// // SocketLabsDeliveredEvent struct
+// type SocketLabsDeliveredEvent struct {
+// 	SocketLabsBaseEvent
+// 	Response  string         `json:"Response"`
+// 	LocalIp   string         `json:"LocalIp"`
+// 	RemoteMta string         `json:"RemoteMta"`
+// 	Data      SocketLabsData `json:"Data"`
+// }
 
-// SocketLabsDeferredEvent struct
-type SocketLabsDeferredEvent struct {
-	SocketLabsBaseEvent
-	FromAddress  string         `json:"FromAddress"`
-	DeferralCode int            `json:"DeferralCode"`
-	Reason       string         `json:"Reason"`
-	Data         SocketLabsData `json:"Data"`
-}
+// // SocketLabsDeferredEvent struct
+// type SocketLabsDeferredEvent struct {
+// 	SocketLabsBaseEvent
+// 	FromAddress  string         `json:"FromAddress"`
+// 	DeferralCode int            `json:"DeferralCode"`
+// 	Reason       string         `json:"Reason"`
+// 	Data         SocketLabsData `json:"Data"`
+// }
 
-// SocketLabsQueuedEvent struct
-type SocketLabsQueuedEvent struct {
-	SocketLabsBaseEvent
-	FromAddress string         `json:"FromAddress"`
-	Subject     string         `json:"Subject"`
-	MessageSize int            `json:"MessageSize"`
-	ClientIp    string         `json:"ClientIp"`
-	Source      string         `json:"Source"`
-	Data        SocketLabsData `json:"Data"`
-}
+// // SocketLabsQueuedEvent struct
+// type SocketLabsQueuedEvent struct {
+// 	SocketLabsBaseEvent
+// 	FromAddress string         `json:"FromAddress"`
+// 	Subject     string         `json:"Subject"`
+// 	MessageSize int            `json:"MessageSize"`
+// 	ClientIp    string         `json:"ClientIp"`
+// 	Source      string         `json:"Source"`
+// 	Data        SocketLabsData `json:"Data"`
+// }
 
-// Supporting struct for Data field
-type SocketLabsData struct {
-	Meta []SocketLabsMeta `json:"Meta"`
-	Tags []string         `json:"Tags"`
-}
+// // Supporting struct for Data field
+// type SocketLabsData struct {
+// 	Meta []SocketLabsMeta `json:"Meta"`
+// 	Tags []string         `json:"Tags"`
+// }
 
-// Supporting struct for Meta field
-type SocketLabsMeta struct {
-	Key   string `json:"Key"`
-	Value string `json:"Value"`
-}
+// // Supporting struct for Meta field
+// type SocketLabsMeta struct {
+// 	Key   string `json:"Key"`
+// 	Value string `json:"Value"`
+// }
