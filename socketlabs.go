b809301ea8c1d3255f9f831fc171700db1b6b614
Nick Martinez <nicholasmartinez@gmail.com>
2024-09-08 07:44:40 -0700

    socket labs working

--- socketlabs.go
+++ socketlabs.go
@@ -14,37 +14,39 @@ import (
 )
 
 func SendEmailWithSocketLabs(emailMessage EmailMessage) error {
-	// Use credentials from the EmailMessage
 	serverID, _ := strconv.Atoi(emailMessage.Credentials.SocketLabsServerID)
 	apiKey := emailMessage.Credentials.SocketLabsAPIKey
 
 	client := injectionapi.CreateClient(serverID, apiKey)
 	errorHandler := NewSocketLabsErrorHandler()
 
-	// Generate X-xsMessageId
 	xxsMessageId := generateXxsMessageId(apiKey)
 
-	// Iterate over each recipient in the "To" field
-	for _, to := range emailMessage.To {
-		// Create the email message for each recipient
+	// Parse sections
+	parsedSections := parseSectionsDynamic(emailMessage.Sections)
+
+	// Process personalizations
+	for _, personalization := range emailMessage.Personalizations {
+		// Process content for each personalization
+		processedContent := processContent(emailMessage.Content, personalization.Substitutions, parsedSections)
+
 		basic := message.BasicMessage{
-			Subject: emailMessage.Subject,
+			Subject: personalization.Subject,
 			From: message.EmailAddress{
 				EmailAddress: emailMessage.From.Email,
 				FriendlyName: emailMessage.From.Name,
 			},
-			PlainTextBody: emailMessage.TextBody,
-			HtmlBody:      emailMessage.HtmlBody,
+			PlainTextBody: getContentByType(processedContent, "text/plain"),
+			HtmlBody:      getContentByType(processedContent, "text/html"),
 		}
 
-		// Add the recipient with a friendly name
-		basic.AddToEmailAddress(to.Email)
+		// Add the recipient
+		basic.AddToEmailAddress(personalization.To.Email)
 
 		// Add CC and BCC recipients
 		for _, cc := range emailMessage.Cc {
 			basic.AddCcEmailAddress(cc)
 		}
-
 		for _, bcc := range emailMessage.Bcc {
 			basic.AddBccEmailAddress(bcc)
 		}
@@ -64,39 +66,28 @@ func SendEmailWithSocketLabs(emailMessage EmailMessage) error {
 			basic.Attachments = append(basic.Attachments, socketLabsAttachment)
 		}
 
-		// Initialize a map to store all headers
+		// Add headers
 		allHeaders := make(map[string]string)
-
-		// Add the custom X-xsMessageId header
 		allHeaders["X-xsMessageId"] = xxsMessageId
-
-		// Add all headers from emailMessage.Headers
 		for key, value := range emailMessage.Headers {
 			allHeaders[key] = value
 		}
-
-		// Clear existing CustomHeaders to avoid duplication
 		basic.CustomHeaders = nil
-
-		// Add all headers to CustomHeaders
 		for key, value := range allHeaders {
 			basic.CustomHeaders = append(basic.CustomHeaders, message.CustomHeader{Name: key, Value: value})
 		}
 
-		log.Printf("%v", &basic)
 		// Send the email
 		res, err := client.SendBasic(&basic)
-
 		if err != nil {
-			errorHandler.HandleSendError(to.Email, err, &res)
+			errorHandler.HandleSendError(personalization.To.Email, err, &res)
 			return fmt.Errorf("failed to send email: %v", err)
 		}
 		if res.Result != injectionapi.SendResultSUCCESS {
 			err := fmt.Errorf("send unsuccessful: %s - %s", res.Result.ToString(), res.Result.ToResponseMessage())
-			errorHandler.HandleSendError(to.Email, err, &res)
+			errorHandler.HandleSendError(personalization.To.Email, err, &res)
 			return err
 		}
-
 	}
 
 	return nil
